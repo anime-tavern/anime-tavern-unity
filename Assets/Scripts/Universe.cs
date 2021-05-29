3a4dbd18def93f44a8abdd3721fa8df3ddb9c4d3
nox7 <17110935+nox7@users.noreply.github.com>
2021-05-29 10:23:13 -0500

    Calculate world grid load time

--- Assets/Scripts/Universe.cs
+++ Assets/Scripts/Universe.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Universe : MonoBehaviour
@@ -7,13 +8,17 @@ public class Universe : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+        UnityEngine.Debug.Log("Loading world grid");
         WorldGrid.LoadMapDataFromPhysicalTiles();
+        stopWatch.Stop();
+        long ts = stopWatch.ElapsedMilliseconds;
+        UnityEngine.Debug.Log("World grid loaded in " + ts + "ms");
 
         GameObject spawnLocationMarker = GameObject.FindGameObjectWithTag("SpawnLocation");
         Vector3 nearestTileCenter = WorldGrid.worldPositionToNearestTileCenter(spawnLocationMarker.transform.position);
         Tile spawnTile = WorldGrid.getTileAtWorldPositionOnLayer(nearestTileCenter, 0);
-        Debug.Log(spawnTile);
 
         GameObject cam = GameObject.FindGameObjectWithTag("MainCamera");
         GameObject player = PlayerBuilder.getBasePlayerObject();
@@ -22,7 +27,6 @@ public class Universe : MonoBehaviour
         player.GetComponent<PlayerController>().playerCamera = cam;
         Vector3 camPosition = new Vector3(0, 12, -10);
         cam.transform.LookAt(player.transform);
-        Debug.Log("spanwed");
     }
 
     // Update is called once per frame
